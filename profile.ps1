1c7c5a6091fed55fe38a075713b0e47849ef09b8
Ben C Robinson <mintyben@users.noreply.github.com>
2014-03-26 09:19:08 +0000

    Added find-infiles to profile.ps1

--- profile.ps1
+++ profile.ps1
@@ -18,3 +18,94 @@ function prompt
 
     Write-Output ([datetime]::Now.ToString('s') + '>');
 }
+
+function Get-RelativePath
+{
+    [CmdletBinding()]
+    param
+    (
+        [Parameter(Position=0, Mandatory=$True)]
+        [string] $Root,
+        
+        [Parameter(Position=1, Mandatory=$True)]
+        [string] $Path
+    )
+    
+    $baseUri = [Uri](Resolve-Path $Root).Path;
+    $relativeUri = [Uri](Resolve-Path $Path).Path;
+    $relativeString = $baseUri.MakeRelative($relativeUri);
+
+    Write-Output ('./' + $relativeString);
+}
+
+function Find-InFiles
+{
+    [CmdletBinding()]
+    param
+    (
+        [Parameter(Position=0, Mandatory=$True)]
+        [string] $Path,
+
+        
+        [Parameter(Position=1, Mandatory=$True)]
+        [string] $Filter,
+        
+        [Parameter(Position=2, Mandatory=$True)]
+        [string] $Match,
+        
+        [switch] $Recurse,
+        [switch] $OutLines,
+        [switch] $OutValues
+    )
+    
+    Write-Verbose "Looking for $Match in $Filter files";
+
+    $regex = new-object -TypeName System.Text.RegularExpressions.Regex -ArgumentList ($Match, [System.Text.RegularExpressions.RegexOptions]::IgnoreCase) ;
+      
+    $basePath = Resolve-Path $Path;
+
+    Get-ChildItem -Path $Path -Filter $Filter -Recurse:$Recurse -File `
+        | ForEach-Object {
+            if(!$_.PSIsContainer -and (Test-Path $_.FullName))
+            {
+                Write-Verbose "Checking file $(Get-RelativePath $basePath $_.FullName)"; 
+
+                try
+                {
+                    if($OutLines)
+                    {
+                        $lines = [IO.File]::ReadAllLines($_.FullName) | Where-Object {$regex.IsMatch($_)};
+                            
+                        if ($lines.Count -gt 0)
+                        {
+                            Write-Output ([pscustomobject]@{Path=$_.FullName; Lines=$lines});
+                        }
+                    }
+                    elseif($OutValues)
+                    {
+                        $text = [IO.File]::ReadAllText($_.FullName);
+
+                        $captures = $regex.Matches($text);
+                        
+
+                        if ($captures.Count -gt 0)
+                        {
+                            $values = $captures | ForEach-Object { @($_.Groups | ForEach-Object Value) };
+                            Write-Output ([pscustomobject]@{Path=$_.FullName; Captures=$values});
+                        }
+                    }
+                    else
+                    {
+                        if ([IO.File]::ReadAllText($_.FullName) -match $Match)
+                        {
+                            Write-Output $_;
+                        }   
+                    }
+                }
+                catch
+                {}
+            }
+        } | Write-Output;
+}
+
+Set-Alias -Name pGrep -Value Find-InFiles;
